c7d16c4ee7af9e199158451be8482fd0e5e21b7c
Brady House <bradyhouse@gmail.com>
2013-11-04 08:45:47 -0600

    Added baseline version of MP3 project code

---
+++ Houseknecht MP3/build/generated-sources/ap-source-output/domain/BaseEntity_.java
@@ -0,0 +1,15 @@
+package domain;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2013-11-03T20:47:10")
+@StaticMetamodel(BaseEntity.class)
+public class BaseEntity_ { 
+
+    public static volatile SingularAttribute<BaseEntity, Integer> id;
+    public static volatile SingularAttribute<BaseEntity, Date> activityDate;
+
+}
